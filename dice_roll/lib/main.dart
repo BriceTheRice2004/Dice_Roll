5ce2366e5e2eba2f2d5e36de095efcca18e1eaf0
BriceThu47400935 <brice.thu@gmail.com>
2024-03-16 19:30:21 +1100

    Added Gradient

--- dice_roll/lib/main.dart
+++ dice_roll/lib/main.dart
@@ -5,7 +5,14 @@ void main() {
     MaterialApp(
       home: Scaffold(
         body: Container(
-          decoration: const BoxDecoration(color: Colors.deepPurple),
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 0, 253, 211),
+                Color.fromARGB(255, 87, 236, 131)
+              ],
+            ),
+          ),
           child: const Center(
             child: Text('Roll the Dice'),
           ),
