9ace9909183ddc6446135d10badcdae7cc439f40
BriceThu47400935 <brice.thu@gmail.com>
2024-03-16 19:34:51 +1100

    added directional gradient

--- dice_roll/lib/main.dart
+++ dice_roll/lib/main.dart
@@ -6,12 +6,11 @@ void main() {
       home: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 0, 253, 211),
-                Color.fromARGB(255, 87, 236, 131)
-              ],
-            ),
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 0, 253, 211),
+              Color.fromARGB(255, 87, 236, 131),
+              Color.fromARGB(255, 188, 245, 82)
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
           ),
           child: const Center(
             child: Text('Roll the Dice'),
