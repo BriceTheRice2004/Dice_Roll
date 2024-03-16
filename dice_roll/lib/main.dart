81d9bb0d84c25b26bd0420b8e98bad319e0916c3
BriceThu47400935 <brice.thu@gmail.com>
2024-03-17 03:02:13 +1100

    Creating out own widgets
    Moved gradient stuff

--- dice_roll/lib/main.dart
+++ dice_roll/lib/main.dart
@@ -4,26 +4,35 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(colors: [
-              Color.fromARGB(255, 0, 253, 211),
-              Color.fromARGB(255, 87, 236, 131),
-              Color.fromARGB(255, 188, 245, 82)
-            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
-          ),
-          child: const Center(
-            child: Text(
-              'Roll the Dice',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w600,
-                  fontFamily: 'Davish',
-                  fontSize: 25),
-            ),
-          ),
-        ),
+        body: AppGradientContainer(),
       ),
     ),
   );
+}
+
+//Like creating classes
+class AppGradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    //What gets returned on screen
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 0, 253, 211),
+          Color.fromARGB(255, 87, 236, 131),
+          Color.fromARGB(255, 188, 245, 82)
+        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+      ),
+      child: const Center(
+        child: Text(
+          'Roll the Dice',
+          style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w600,
+              fontFamily: 'Davish',
+              fontSize: 25),
+        ),
+      ),
+    );
+  }
 }
